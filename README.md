d372b025f8e4d5a36fa716dd336846145d95882a
Heron Yang <heron.yang.tw@gmail.com>
2018-04-14 14:04:13 -0700

    Adds merged data and the script for it

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vibration + Footprint
+
+## Preprocessing
+
+Merge multiple raw data located in `./data/` into one single file per test:
+
+    $ ./merge_raw.py
--- merge_raw.py
+++ merge_raw.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
-import sys
-DIR = sys.argv[1]
+import os
+import itertools
 
-s = ""
-i = 0
+DATA_ROOT = "./data/"
+DATA_MERGE_ROOT = "./merged_data/"
 
-while True:
+def merge_all():
+    for subdir in os.listdir(DATA_ROOT):
+        print("Found %s" % subdir)
+        for index in itertools.count():
+            input_dir = DATA_ROOT + subdir + "/" + subdir + "-" + str(index + 1)
+            output_filepath = "%s%s/%02d" % (DATA_MERGE_ROOT, subdir, index + 1)
+            if os.path.exists(input_dir):
+                merge_dir(input_dir, output_filepath)
+            else:
+                break
 
-    filename = "data-" = i
-    if not os.path.exists(filename):
-        print("End, %d is done" % i)
-        break
-    f = open(filename)
-    s += f.read()
-    f.close()
-    i += 1
+def merge_dir(input_dir, output_filepath):
 
-f = open(filename, "w")
-f.write(s)
-f.close()
+    data = ""
+
+    for index in itertools.count():
+        input_filepath = "%s/data_%d" % (input_dir, index)
+        if not os.path.exists(input_filepath):
+            break
+        with open(input_filepath) as fin:
+            data += fin.read()
+
+    mkdir(os.path.dirname(output_filepath))
+    with open(output_filepath, "w") as fout:
+        fout.write(data)
+
+    print("%s => %s done" % (input_dir, output_filepath))
+
+def mkdir(dir_path):
+    os.makedirs(dir_path, exist_ok=True)
+
+if __name__ == "__main__":
+    merge_all()
