6233745b2349f3d67ca5c927b74af2b95364409d
Heron Yang <heron.yang.tw@gmail.com>
2018-04-15 15:37:25 -0700

    Add input part for spot_event_svm

--- spot_event_svm.py
+++ spot_event_svm.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
+import numpy as np
+from sklearn import svm
 
-def get_io_files():
+TRAIN_ROOT = "./svm/train/"
+TEST_ROOT = "./svm/test/"
 
-    if len(sys.argv[1:]) != 3:
-        print("Usage: ./spot_event_svm.py train_data_folder test_data_folder")
-        sys.exit(-1)
+def get_input():
+
+    # Train
+    X_0 = np.loadtxt(TRAIN_ROOT + "0/metric.txt")
+    X_1 = np.loadtxt(TRAIN_ROOT + "1/metric.txt")
+    X = np.vstack((X_0, X_1))
+    y = np.array([0] * len(X_0) + [1] * len(X_1))
+
+    # Test
+    X_test_0 = np.loadtxt(TEST_ROOT + "0/metric.txt")
+    X_test_1 = np.loadtxt(TEST_ROOT + "1/metric.txt")
+    X_test = np.vstack((X_test_0, X_test_1))
+    y_test = np.array([0] * len(X_test_0) + [1] * len(X_test_1))
+
+    return X, y, X_test, y_test
+
+def main():
+    model = svm.SVC(kernel="linear", C=1, gamma=1)
+    X, y, X_test, y_test = get_input()
+
+if __name__ == "__main__":
+    main()
